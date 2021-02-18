fa97250a6e03c1504b83926f27149f99d97670a2
KMorris63 <72368174+KMorris63@users.noreply.github.com>
2021-02-17 23:00:43 -0700

    DDL Script
    DDL for minesweeper milestone user database table

---
+++ milestoneDatabase.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[users] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [firstName] NVARCHAR (50) NOT NULL,
+    [lastName]  NVARCHAR (50) NOT NULL,
+    [gender]    NVARCHAR (50) NULL,
+    [age]       INT           NULL,
+    [state]     NVARCHAR (50) NULL,
+    [email]     NVARCHAR (50) NOT NULL,
+    [username]  NVARCHAR (50) NOT NULL,
+    [password]  NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([username] ASC)
+);
