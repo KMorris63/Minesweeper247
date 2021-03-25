47559daee9114343b38b4eb37de92eeb85f705ca
KMorris63 <72368174+KMorris63@users.noreply.github.com>
2021-03-24 17:15:11 -0700

    Difficulty Levels
    Difficulty levels implemented with right click, flood fill, and partial page updates.

---
+++ Milestone/Milestone/Controllers/CellController.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Milestone.Models;
+using Milestone.Views.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+/**
+ * Kacey Morris
+ * Alex Vergara
+ * March 15 2021
+ * CST 247
+ * Milestone 3 - Flags and Game Over
+ * CellController.cs
+ * 
+ * This class controls all background for the game play. It sets up the game and returns views. 
+ * 
+ * This is our own work, as influenced by class time and video tutorials for previous projects.
+ */
+
+namespace Milestone.Controllers
+{
+    public class CellController : Controller
+    {
+        // reference to the class Board. Contains the values of the board.
+        static Board myBoard;
+        static GameBusinessService bs;
+
+        // booleans to allow for the game to continue and for a win
+        string winIndicator = "tbd";
+
+        public IActionResult Index(string level)
+        {
+            int size;
+            int difficulty;
+            // pass the size and difficulty depending on level to create the board
+            switch (level)
+            {
+                // easy has 10 bombs on a 10 x 10 grid
+                case "easy":
+                    size = 10;
+                    difficulty = 10;
+                    break;
+                // medium has 12 and 10
+                case "medium":
+                    size = 12;
+                    difficulty = 10;
+                    break;
+                // hard has 15 and 15
+                case "hard":
+                    size = 15;
+                    difficulty = 15;
+                    break;
+                // expert has 18 and 18
+                case "expert":
+                    size = 18;
+                    difficulty = 18;
+                    break;
+                default:
+                    size = 5;
+                    difficulty = 5;
+                    break;
+            }
+            myBoard = new Board(size, difficulty);
+            bs = new GameBusinessService(myBoard);
+
+            // set up the bombs
+            bs.setupLiveNeighbors();
+            // calculate all live neighbors
+            bs.calculateLiveNeighbors();
+            // set the number of bombs to what was passed through
+            bs.GetBoard().SetNumberOfBombs(10);
+
+            // pass list of cells to view
+            return View("Index", bs.GetBoard().GetCellList());
+        }
+        public IActionResult HandleButtonClick(int cellNum)
+        {
+            Cell currentCell = bs.GetBoard().GetCellList().ElementAt(cellNum);
+            var buttonHTMLString = "";
+
+            // only allow left click for non flagged cells
+            if (!currentCell.GetFlagged())
+            {
+                // visit cell
+                currentCell.SetVisited(true);
+
+                // call flood fill on that cell spot
+                // ONLY if there are no neighbors
+                if (currentCell.GetNeighbors() == 0)
+                {
+                    // do the flood fill
+                    bs.floodFill(currentCell.GetRowNumber(), currentCell.GetColumnNumber()); 
+                    
+                }
+
+                // if they selected a bomb, end the game and let them know
+                if (currentCell.GetLive())
+                {
+                    // set win to false, they lost
+                    winIndicator = "false";
+                }
+                // if they cleared all non bomb spots, end the game and display win
+                else if (bs.checkForWin())
+                {
+                    // set win to true, they won
+                    winIndicator = "true";
+                }
+            }
+
+            // if they have not won or loss yet, send an update for the grid without displaying all values
+            if (winIndicator == "tbd")
+            {
+                // get the html string for entire grid
+                buttonHTMLString = RenderRazorViewToString(this, "ShowGrid", bs.GetBoard().GetCellList());
+            }
+            else
+            {
+                // get the html string for entire grid showing all bombs and squares
+                buttonHTMLString = RenderRazorViewToString(this, "ShowGridFinal", bs.GetBoard().GetCellList());
+            }
+            // redisplay the button grid
+            return Json(new { part1 = buttonHTMLString, part2 = winIndicator });
+        }
+
+        // action for right button click 
+        public IActionResult onRightButtonClick(int cellNum)
+        {
+            // int cellNum = buttonNumber;
+            Cell currentCell = bs.GetBoard().GetCellList().ElementAt(cellNum);
+
+            // only change the flag if the cell is not visited
+            // we don't want them to not be able to win because they have a flag that doesn't show
+            if (!currentCell.GetVisited())
+            {
+                // set the flagged state to the opposite of what it was before
+                currentCell.SetFlagged(!currentCell.GetFlagged());
+            }
+            
+            // get the html string for one button
+            var buttonHTMLString = RenderRazorViewToString(this, "ShowOneButton", currentCell);
+
+            // we want to return just the cell that was altered as a partial page update
+            return Json(new { part1 = buttonHTMLString, part2 = winIndicator });
+        }
+
+        // from the link in the activity instructions
+        public static string RenderRazorViewToString(Controller controller, string viewName, object model = null)
+        {
+            controller.ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                IViewEngine viewEngine =
+                    controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as
+                        ICompositeViewEngine;
+                ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, false);
+
+                ViewContext viewContext = new ViewContext(
+                    controller.ControllerContext,
+                    viewResult.View,
+                    controller.ViewData,
+                    controller.TempData,
+                    sw,
+                    new HtmlHelperOptions()
+                );
+                viewResult.View.RenderAsync(viewContext);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
---
+++ Milestone/Milestone/Views/Services/Business/GameBusinessService.cs
@@ -0,0 +1,221 @@
+﻿using Milestone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/**
+ * Kacey Morris
+ * Alex Vergara
+ * March 15 2021
+ * CST 247
+ * Milestone 3 - Flags and Game Over
+ * GameBusinessService.cs
+ * 
+ * This class contains all the game rules and logic to play minesweeper. 
+ * 
+ * This is my own work, as influenced by class time and video tutorials for previous projects.
+ */
+
+namespace Milestone.Views.Services.Business
+{
+    public class GameBusinessService
+    {
+        // pass the board of cells
+        private static Board myBoard;
+        // data constructor
+        public GameBusinessService(Board board)
+        {
+            myBoard = board;
+        }
+
+        // GAME LOGIC
+        // assign cells in the grid as bombs
+        public void setupLiveNeighbors()
+        {
+            // random to generate location of bombs
+            Random rand = new Random();
+
+            // generate a random row and column, from 0 to the size of the board
+            int randomRow = rand.Next(myBoard.GetSize());
+            int randomCol = rand.Next(myBoard.GetSize());
+
+            // the percentage will be a double value that will determine the number of bombs
+            double percentage = myBoard.GetDifficulty() / 100.0;
+
+            // the number of bombs will be an integer determined by the total number of cells on the board
+            // multiplied by the percentage of cells that are bombs based on the difficulty, forced to be an int
+            int numLiveCells = Convert.ToInt32((myBoard.GetSize() * myBoard.GetSize()) * percentage);
+
+            // for however many bombs there are, choose random locations for them
+            for (int i = 0; i < numLiveCells; i++)
+            {
+                // if the cell is already live, we want to keep generating more random locations
+                while (myBoard.GetGrid()[randomRow, randomCol].GetLive())
+                {
+                    randomRow = rand.Next(myBoard.GetSize());
+                    randomCol = rand.Next(myBoard.GetSize());
+                }
+                // set that cell to live
+                myBoard.GetGrid()[randomRow, randomCol].SetLive(true);
+            }
+        }
+
+        // calculate the live neighbors
+        public void calculateLiveNeighbors()
+        {
+            // loop through all cells on the board
+            for (int i = 0; i < myBoard.GetSize(); i++)
+            {
+                for (int j = 0; j < myBoard.GetSize(); j++)
+                {
+                    Cell currentCell = myBoard.GetGrid()[i, j];
+                    // if the current cell is live, it has 9 live neighbors as per the directions
+                    if (currentCell.GetLive())
+                    {
+                        currentCell.SetNeighbors(9);
+                    }
+                    else
+                    {
+                        // set default values off the board so an accidental neighbor is not counted 
+                        int manipulatedRow = -1;
+                        int manipulatedCol = -1;
+                        // set the neighbor counter to 0 for each cell to start
+                        int neighborCount = 0;
+                        // there are 8 possible neighbors for each cell, so loop through 8 times
+                        for (int x = 0; x < 8; x++)
+                        {
+                            // on each loop through, check a different neighboring cell for a bomb
+                            switch (x)
+                            {
+                                // each of these cases chooses a cell immediately surrounding the current cell
+                                // to the right and down
+                                case 0:
+                                    manipulatedRow = currentCell.GetRowNumber() + 1;
+                                    manipulatedCol = currentCell.GetColumnNumber() + 1;
+                                    break;
+                                // to the right
+                                case 1:
+                                    manipulatedRow = currentCell.GetRowNumber() + 1;
+                                    manipulatedCol = currentCell.GetColumnNumber();
+                                    break;
+                                // to the right and up
+                                case 2:
+                                    manipulatedRow = currentCell.GetRowNumber() + 1;
+                                    manipulatedCol = currentCell.GetColumnNumber() - 1;
+                                    break;
+                                // up
+                                case 3:
+                                    manipulatedRow = currentCell.GetRowNumber();
+                                    manipulatedCol = currentCell.GetColumnNumber() - 1;
+                                    break;
+                                // down
+                                case 4:
+                                    manipulatedRow = currentCell.GetRowNumber();
+                                    manipulatedCol = currentCell.GetColumnNumber() + 1;
+                                    break;
+                                // left and down
+                                case 5:
+                                    manipulatedRow = currentCell.GetRowNumber() - 1;
+                                    manipulatedCol = currentCell.GetColumnNumber() + 1;
+                                    break;
+                                // left
+                                case 6:
+                                    manipulatedRow = currentCell.GetRowNumber() - 1;
+                                    manipulatedCol = currentCell.GetColumnNumber();
+                                    break;
+                                // left and up
+                                case 7:
+                                    manipulatedRow = currentCell.GetRowNumber() - 1;
+                                    manipulatedCol = currentCell.GetColumnNumber() - 1;
+                                    break;
+                                default:
+                                    break;
+                            }
+                            // after the neighboring cell is selected, make sure it is on the board and check if it is live
+                            if ((manipulatedRow < myBoard.GetSize()) && (manipulatedRow >= 0) && (manipulatedCol < myBoard.GetSize()) && (manipulatedCol >= 0)
+                                && myBoard.GetGrid()[manipulatedRow, manipulatedCol].GetLive())
+                            {
+                                // if live, up the bomb counter
+                                neighborCount++;
+                            }
+                        }
+                        // after all neigboring cells are looped through, set the neighbor count for that cell
+                        currentCell.SetNeighbors(neighborCount);
+                    }
+                }
+            }
+        }
+
+        // checks for a winning condition
+        public Boolean checkForWin()
+        {
+            for (int i = 0; i < myBoard.GetSize(); i++)
+            {
+                for (int j = 0; j < myBoard.GetSize(); j++)
+                {
+                    // if there are spots which are not bombs and not visited, the user has not won
+                    if (!myBoard.GetGrid()[i, j].GetVisited() && !myBoard.GetGrid()[i, j].GetLive())
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        // flood fill
+        public void floodFill(int row, int col)
+        {
+            // different possible combination of surrounding cells
+            int[] xMove = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] yMove = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+            // same logic as calculate live neighbors, but condensed
+            int next_x, next_y;
+
+            // make sure we have visited the cell
+            // if the cell has neighbors, those will already show up on the print now
+            myBoard.GetGrid()[row, col].SetVisited(true);
+
+            // if the spot had been flagged, set it to not flagged and decrement the non bombs discovered
+            if (myBoard.GetGrid()[row, col].GetFlagged())
+            {
+                myBoard.GetGrid()[row, col].SetFlagged(false);
+                myBoard.SetNonBombsDiscovered(myBoard.GetNonBombsDiscovered() - 1);
+            }
+
+            // if it is blank, we want to check its surrounding cells too
+            if (myBoard.GetGrid()[row, col].GetNeighbors() == 0)
+            {
+                // for all surrounding cells
+                for (int k = 0; k < 8; k++)
+                {
+                    next_x = row + xMove[k];
+                    next_y = col + yMove[k];
+                    // check to make sure it is safe (in the grid and not yet visited)
+                    // Console.WriteLine("Checking if (" + next_x  + ", " + next_y + ") is safe.");
+                    if (safeSquare(next_x, next_y))
+                    {
+                        // Console.WriteLine("Safe.");
+                        // recursion, call this method on the next new choices
+                        floodFill(next_x, next_y);
+                    }
+                }
+            }
+        }
+
+        // is this square on the board?
+        bool safeSquare(int x, int y)
+        {
+            // check to see if x, y is on the board. no out of bounds index errors allowed
+            // do we care if the cell has already been visited? We do. Stack overflow.
+            return (x >= 0 && x < myBoard.GetSize() && y >= 0 && y < myBoard.GetSize() && !myBoard.GetGrid()[x, y].GetVisited());
+        }
+
+        public Board GetBoard()
+        {
+            return myBoard;
+        }
+    }
+}
